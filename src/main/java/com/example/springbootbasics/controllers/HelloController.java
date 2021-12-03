a9c09d5e3db4c6c979c30b34f9f1cc1c7513b65f
codingkiddo <codingkiddo@gmail.com>
2021-12-03 11:48:12 +0530

    first commit

---
+++ src/main/java/com/example/springbootbasics/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.springbootbasics.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+//	@RequestMapping("hello")
+//	public String sayHello(HttpSession session, HttpServletResponse res, HttpServletRequest req) {
+//		System.out.println("Hello !!!!!");
+//		System.out.println(this.toString());
+//		System.out.println(req);
+//		System.out.println(res);
+//		System.out.println(session);
+//		return "hello";
+//	}
+	
+	
+	@RequestMapping("hello")
+	public String sayHello(HttpSession session, String name) {
+		System.out.println("Hello !!!!!");
+		System.out.println(this.toString());
+		System.out.println("Name: " + name);
+		session.setAttribute("name", name);
+		System.out.println(session);
+		return "hello";
+	}
+	
+}
